27933d77f83e0ee78fbebb82d7c053ef4392699b
Galor Siboni <GalorSiboni@gmail.com>
2020-11-22 16:07:33 +0200

    Upper and Lower Cases checks Functions info

--- src/main/java/com/example/currencycodegetterlibrary/currencyCodeGetter.java
+++ src/main/java/com/example/currencycodegetterlibrary/currencyCodeGetter.java
@@ -7,6 +7,11 @@ import java.util.Map;
 import java.util.TreeMap;
 
     public class currencyCodeGetter {
+
+        /**
+         * This function return Map of Strings, when the keys is the countrys and the values is there currency codes
+         * @return Map<String, String>
+         */
         public Map<String, String> getAvailableCurrencies() {
             Locale[] locales = Locale.getAvailableLocales();
 
@@ -24,27 +29,67 @@ import java.util.TreeMap;
             return currencies;
         }
 
+        /**
+         * This function return a ArrayList Countries as Strings when u give her a String of a Currency Code.
+         * For example: give this currency code : "EUR" will return all the Countries that use a EURO.(The currency of the European Union)
+         * @param cd String
+         * @return
+         */
         public ArrayList<String> getCountryByCurrencyCode(String cd) {
+            String input = cd;
+            if (isEnglish( input )) {
+                input = input.toUpperCase();
+            }
             Map<String, String> currencies = getAvailableCurrencies();
-            ArrayList<String> cuntries = new ArrayList<>();
-            if(currencies.values().contains( cd )) {
+            ArrayList<String> countries = new ArrayList<>();
+            if(currencies.values().contains( input )) {
                 for (Map.Entry<String, String> entry : currencies.entrySet()) {
-                    if (entry.getValue().equals(cd)) {
-                        cuntries.add(entry.getKey());
+                    if (entry.getValue().equals(input)) {
+                        countries.add(entry.getKey());
                     }
                 }
             }
-            if (cuntries.isEmpty())
+            if (countries.isEmpty())
                 return null;
-            else return cuntries;
+            else return countries;
         }
 
+        /**
+         * This function return String of a Currency Code when u give ger a String of a Country in the phone local language.
+         * For example: give this country if your phone set to English: "Brazil" and the function will return "BRL" (The currency code of the Brazilian real).
+         * @param country String
+         * @return
+         */
         public String getCurrencyCodeByCountry(String country) {
+            String input = country;
+            if (isEnglish( input )) {
+                String temp = input.toLowerCase();
+                input = temp.substring( 0, 1 ).toUpperCase() + temp.substring( 1 );
+            }
             Map<String, String> currencies = getAvailableCurrencies();
-            String cd = currencies.get(country);
+            String cd = currencies.get(input);
             if (cd != null)
                 return cd;
             return "Not Found!";
+        }
+        private boolean isEnglish(String text) {
+            boolean onlyEnglish = false;
+
+            for (char character : text.toCharArray()) {
+
+                if (Character.UnicodeBlock.of(character) == Character.UnicodeBlock.BASIC_LATIN
+                        || Character.UnicodeBlock.of(character) == Character.UnicodeBlock.LATIN_1_SUPPLEMENT
+                        || Character.UnicodeBlock.of(character) == Character.UnicodeBlock.LATIN_EXTENDED_A
+                        || Character.UnicodeBlock.of(character) == Character.UnicodeBlock.GENERAL_PUNCTUATION) {
+
+                    onlyEnglish = true;
+                } else {
+
+                    onlyEnglish = false;
+                }
+            }
+
+            return onlyEnglish;
         }
     }
 
